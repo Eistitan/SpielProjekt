78bfd29e16b6e5537339ec388af82a5f72d57f95
Dietrich Kirsch <eistitan@yahoo.com>
2022-09-14 15:30:06 +0200

    Projektdateien hinzufügen.

---
+++ Person.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpielProjekt
+{
+    public class Person
+    {
+        public string Klasse; //Spieler oder Gegner
+        public string Name;
+        public int Staerke;
+        public int Vitality;
+        public int HP;
+        //public int HP = 1 * Staerke + 3 * Vitality;
+
+
+        public void Darstellung()
+        {
+            Console.WriteLine($"Der {Klasse} heißt {Name} und hat {HP} Lebenspunkte.");
+        }
+        public Person(string Klasse, string Name, int Staerke, int Vitality)
+        {
+            this.Klasse = Klasse;
+            this.Name = Name;
+            this.Staerke = Staerke;
+            this.Vitality = Vitality;
+            this.HP = 1 * Staerke + 3 * Vitality;
+             
+        }
+
+       
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Xml.Linq;
+
+
+
+
+namespace SpielProjekt
+{
+	 public class Game
+	{
+		public static void Main()
+		{
+            Gegner();
+            Console.WriteLine("");
+            Spieler();
+            
+        }
+
+        public static Person Gegner()
+        {
+            Console.WriteLine("Name");
+            string Name = Console.ReadLine();
+            Console.WriteLine("Stärke");
+            int Staerke = int.Parse(Console.ReadLine());
+            Console.WriteLine("Vitality");
+            int Vitality = int.Parse(Console.ReadLine());
+            //Opponent.HP = Opponent.Staerke + 3 * Opponent.Vitality;
+            string Klasse = "Gegner";
+
+            Person Opponent = new Person(Klasse, Name, Staerke, Vitality);
+            Opponent.Darstellung();
+
+            return Opponent;
+
+        }
+        public static Person Spieler()
+        {
+            Console.WriteLine("Name");
+            string Name = Console.ReadLine();
+            Console.WriteLine("Stärke");
+            int Staerke = int.Parse(Console.ReadLine());
+            Console.WriteLine("Vitality");
+            int Vitality = int.Parse(Console.ReadLine());
+            //Opponent.HP = Opponent.Staerke + 3 * Opponent.Vitality;
+            string Klasse = "Spieler";
+
+            Person Player = new Person(Klasse, Name, Staerke, Vitality);
+            Player.Darstellung();
+            
+            return Player;
+
+        }
+        
+        //public static int Schlag()
+        //{
+        //    int dmg;
+            
+        //    //Richtung Opponent oder Spieler
+        //    if (true)
+        //    {
+
+        //    }
+        //    //Schaden = 1,5*Staerke - 0,3*Vitality des Gegners
+
+        //    return dmg;
+        //}
+
+
+    }
+}
